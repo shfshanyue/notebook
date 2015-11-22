1effc6b006d9a71454ef74fad32d47fc9654ed91
wangxian <1144542900@qq.com>
2015-11-22 18:15:34 +0800

    largely completed

--- js/note.js
+++ js/note.js
@@ -1,13 +1,15 @@
 var storage = new Storage();
 
+
 var vm = new Vue({
     el: 'body',
     data: {
-        tasks: storage.getTask(),
-        showTasks: storage.getTaskOfCatalog(0),
-        catalogs: storage.getCatalog(),
-        newCatalog: '',
+        tasks: storage.fetchTask(),
+        catalogs: storage.fetchCatalog(),
         taskOfCatalogId: 0,
+        editTask: {},
+        editable: false,
+        newCatalog: '',
         model: false,
     },
     methods: {
@@ -16,29 +18,84 @@ var vm = new Vue({
         },
 
         addCatalog: function() {
-            if (this.newCatalog) {
-                storage.createCatalog(this.newCatalog);
-                this.catalogs = storage.getCatalog();
+            var catalogname = this.newCatalog;
+            if (catalogname) {
+                if (!this.catalogs['length']) {
+                    this.catalogs['length'] = 0;
+                    this.catalogs.catalogs = [];
+                } 
+                var len = this.catalogs.length;
+                var catalog = {
+                    'id': len,
+                    'name': catalogname,
+                    'length': 0,
+                }
+                this.catalogs.length++;
+                this.catalogs.catalogs.push(catalog);
+                Vue.set(this.tasks, len, []);
                 this.newCatalog = '';
                 this.model = false;
             }
         },
-
         addTask: function() {
+            this.editable = true;
+            this.editTask = {};
+            document.getElementsByClassName('note-text')[0].textContent = '';
+            document.getElementsByClassName('edit-content')[0].textContent = '';
+        },
+        saveTask: function() {
+            if (!this.editable) 
+                return;
             var id = this.taskOfCatalogId;
-            storage.createTask(id, 'python', '装饰器的功能');
-            this.showTasks = storage.getTaskOfCatalog(id);
+            var title = document.getElementsByClassName('note-text')[0].textContent.trim();
+            var content = document.getElementsByClassName('edit-content')[0].textContent.trim();
+            if (!this.editTask.title && title) {
+                this.tasks[id].push({
+                    title: title,
+                    content: content,
+                    size: content.length,
+                    createTime: new Date().toLocaleString('en-GB', {hour12:false}),
+                    updateTime: new Date().toLocaleString('en-GB', {hour12:false})
+                });
+            } else {
+                this.editTask.title = title;
+                this.editTask.content = content;
+                this.editTask.size = content.length;
+                this.editTask.updateTime = new Date().toLocaleString('en-GB', {hour12:false});
+            }
+            this.editable = false;
         },
     },
     watch: {
-        taskOfCatalogId: {
-            handler: function(id) {
-                this.showTasks = storage.getTaskOfCatalog(id);
+        catalogs: {
+            handler: function(catalogs) {
+                storage.saveCatalog(catalogs);
+                console.log('catalog save');
             },
+            deep: true,
         },
-        showTasks: {
-            
+        editable: {
+            handler: function() {
+                storage.saveTask(this.tasks);
+                console.log('task save');
+            },
+            deep: true,
+        },
+        tasks: {
+            handler: function(newtasks, oldtasks) {
+                console.log(newtasks);
+                console.log(oldtasks);
+            },
+            deep: true,
         }
     },
-})
+    filter: {
+        'reverse': function(value) {
+            return value.split('').reverse().join('');
+        }
+    }
+});
+
+Vue.config.debug = true;
+
 
--- js/storage.js
+++ js/storage.js
@@ -4,101 +4,31 @@ function Storage() {
 
 Storage.prototype = {
 
-    // Catalog getters/setters
-    createCatalog: function(name) {
-        var catalogs = this.getCatalog();
-        if (catalogs && name) {
-            catalogs[catalogs.length] = {
-                id: catalogs.length,
-                name: name,
-                length: 0,
-            };
-            catalogs.length += 1;
-        } else if (!catalogs) {
-            catalogs = {};
-            catalogs[0] = {
-                id: 0,
-                name: name,
-                length: 0,
-            };
-            catalogs.length = 1;
-        }
+    fetchCatalog: function() {
+        return JSON.parse(this.storage.getItem('catalog')) || {
+            length: 0,
+            catalogs: []
+        };
+    },
+
+    saveCatalog: function(catalogs) {
         this.storage.setItem('catalog', JSON.stringify(catalogs));
     },
 
-    getCatalog: function() {
-        var catalogs = this.storage.getItem('catalog');
-        return catalogs ? JSON.parse(catalogs): null;
+    fetchTask: function() {
+        return JSON.parse(this.storage.getItem('task') || '{}');
     },
 
-    removeCatalog: function(id) {
-        var catalogs = this.getCatalog();
-        if (catalogs[id]) {
-            delete catalogs[id];
-            return true;
-        }
-        return false;
-    },
-
-    createTask: function(pid, title, content) {
-        var tasks = this.getTask();
-        var task = {
-            title: title,
-            content: content,
-            size: content.length,
-            createTime: new Date(),
-            updateTime: new Date(),
-        };
-        if (tasks) {
-            if (!tasks[pid]) {
-                tasks[pid] = {};
-                tasks[pid].length = 0;
-            }
-            task.id = tasks.length;
-            tasks[pid][tasks.length] = task;
-            tasks.length += 1;
-            tasks[pid].length += 1;
-        } else{
-            tasks = {};
-            tasks.length = 1;
-            tasks[pid] = {};
-            task.id = 0;
-            tasks[pid][0] = task;
-            tasks[pid].length = 1;
-        }
+    saveTask: function(tasks) {
         this.storage.setItem('task', JSON.stringify(tasks));
     },
 
-    editTask: function(pid, id, title, content) {
-        var tasks = this.getTask();
-        if (tasks && tasks[pid] && tasks[pid][id]) {
-            tasks[pid][id].title = title;
-            tasks[pid][id].content = content;
-            tasks[pid][id].size = content.length;
-            tasks[pid][id].updateTime = new Date();
-            this.storage.setItem('task', JSON.stringify(tasks));
-        }
-    },
+    // get: function(name) {
+    //     return this.storage.
+    // },
 
-    getTask: function() {
-        var tasks = this.storage.getItem('task');
-        return tasks ? JSON.parse(tasks): {};
-    },
-
-    removeTask: function(pid, id) {
-        var tasks = this.getTask();
-        if (tasks[pid] && tasks[pid][id]) {
-            delete tasks[pid][id];
-            this.storage.setItem('task', JSON.stringify(tasks));
-            return true;
-        }
-        return false;
-    },
-
-    getTaskOfCatalog: function(pid) {
-        var tasks = this.getTask();
-        return tasks ? tasks[pid]: null;
-    }
-
+    // save: function(name, obj) {
+    //     this.storage.setItem(name, JSON.stringify(obj));
+    // }
 }
 
